de18ac4e8c220a6e3d7bbdfa0c517d7dbb7538d8
Eddie Krill <scherenhaenden@gmail.com>
2021-04-24 15:07:09 +0200

    first raw commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Learning_Project01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
